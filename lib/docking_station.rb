bdb7f0146665d9ba76e3642c1005043d09f88755
Maciej Kurek <maciejk77@gmail.com>
2015-04-20 16:54:43 +0100

    Second commit

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,22 @@
 class DockingStation
   def initialize
+    @container = []
   end
 
   def release_bike
-    @bike
+    @container.pop
   end
 
   def dock(bike)
-    @bike = bike
+    raise 'Station full' if @container.count >= 10 
+    @container << bike
   end
 
   def capacity
     @capacity = 10
+  end
+
+  def container
+    @container
   end
 end
--- spec/feature/public_bike_access_spec.rb
+++ spec/feature/public_bike_access_spec.rb
@@ -3,7 +3,8 @@ require 'capybara/rspec'
 feature 'member of a public accesses bike' do
   scenario 'docking station releases a bike that is not broken' do
     docking_station = DockingStation.new
-    bike = docking_station.release_bike
+    bike = Bike.new
+    docking_station.dock(bike)
     expect(bike).not_to be_broken
   end
 
